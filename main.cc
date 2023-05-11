d3230f916e02dfd4b0d587036cbfc735912fc19d
dweller4085 <immbirj@gmail.com>
2023-05-11 16:18:24 +0700

    Implemented random cnf generator / automated testing.

--- main.cc
+++ main.cc
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <string>
+#include <random>
 #include "solver.hh"
 
+std::string generateRandomCnf(u32 varCnt, u32 clauseCnt) {
+    auto cnf = std::string {} + "n " + std::to_string(varCnt) + "\nm " + std::to_string(clauseCnt) + "\n";
+    auto rng = std::mt19937 {};
+    static constexpr char val[3] { '0', '1', '-' };
+    for (u32 i = 0; i < clauseCnt; i += 1) {
+        for (u32 j = 0; j < varCnt; j += 1) {
+            cnf += val[rng () % u32 { 3 }];
+        } cnf += '\n';
+    }
 
-char const * const cnf {R"(
-n 3
-m 4
-01-
-0-1
--11
--00
-)"};
+    return cnf;
+}
 
 int main (int argc, char ** argv) {
-    Solver::init(std::string {cnf}, Solver::Settings {});
-    auto result = Solver::solve();
-    std::cout << (std::string) result << "\n";
+
+    auto cnf = generateRandomCnf(8, 5);
+    Solver::init(cnf, Solver::Settings {});
+    std::cout << cnf << "\n";
+    std::cout << (std::string) Solver::solve() << "\n";
+
+    //for (int i = 0; i < 10; i += 1) {
+    //    Solver::init(generateRandomCnf(64, 64), Solver::Settings {});
+    //    auto result = Solver::solve();
+    //    std::cout << (std::string) result << "\n";
+    //}
 }
